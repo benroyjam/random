437d24a911ac148de15c2f3fcbd052d044f2a712
Arnav Singh <arnavion@gmail.com>
2013-03-01 19:57:38 -0800

    video.pl: Added check for whitelisted channels and other bots.

--- hexchat/video.pl
+++ hexchat/video.pl
@@ -12,6 +12,7 @@ my $httpAsync = HTTP::Async->new();
 my $jsonParser = JSON->new()->utf8();
 my $xmlParser = XML::Simple->new(SuppressEmpty => '');
 my @channels = ( '#sage', '#vodka-subs', '#commie-subs', '#arnavion' );
+my @otherBots = ( 'Belfiore', 'Yotsugi' );
 
 sub httpRequestAsync {
 	my ($url, $callback) = @_;
@@ -34,25 +35,27 @@ sub httpRequestAsync {
 sub handler {
 	my $channel = Xchat::get_info('channel');
 
-	if (
-		($_[0][1] =~ /(?:www\.)?youtube\.com\/watch\?(?:.*?&)?v=([^#& ?]*)/) ||
-		($_[0][1] =~ /(?:www\.)?youtu\.be\/([^#& ?]*)/)
-	) {
-		httpRequestAsync('http://gdata.youtube.com/feeds/api/videos/' . $1 . '?v=2&alt=jsonc', sub {
-			my $json = $jsonParser->decode($_[0]);
-			if (exists $json->{data} && exists $json->{data}->{title}) {
-				Xchat::command("msg $channel Youtube video title: $json->{data}->{title}");
-			}
-		});
-	}
-	
-	elsif ($_[0][1] =~ /www\.nicovideo\.jp\/watch\/([^ ]+)/) {
-		httpRequestAsync('http://ext.nicovideo.jp/api/getthumbinfo/' . $1 . '/title', sub {
-			my $xml = $xmlParser->XMLin($_[1]);
-			if (exists $xml->{thumb} && exists $xml->{thumb}->{title}) {
-				Xchat::command("msg $channel Niconico video title: $xml->{thumb}->{title}");
-			}
-		});
+	if (grep(/^$channel$/i, @channels) && !grep { Xchat::user_info($_) } @otherBots) {
+		if (
+			($_[0][1] =~ /(?:www\.)?youtube\.com\/watch\?(?:.*?&)?v=([^#& ?]*)/) ||
+			($_[0][1] =~ /(?:www\.)?youtu\.be\/([^#& ?]*)/)
+		) {
+			httpRequestAsync('http://gdata.youtube.com/feeds/api/videos/' . $1 . '?v=2&alt=jsonc', sub {
+				my $json = $jsonParser->decode($_[0]);
+				if (exists $json->{data} && exists $json->{data}->{title}) {
+					Xchat::command("msg $channel Youtube video title: $json->{data}->{title}");
+				}
+			});
+		}
+		
+		elsif ($_[0][1] =~ /www\.nicovideo\.jp\/watch\/([^ ]+)/) {
+			httpRequestAsync('http://ext.nicovideo.jp/api/getthumbinfo/' . $1 . '/title', sub {
+				my $xml = $xmlParser->XMLin($_[1]);
+				if (exists $xml->{thumb} && exists $xml->{thumb}->{title}) {
+					Xchat::command("msg $channel Niconico video title: $xml->{thumb}->{title}");
+				}
+			});
+		}
 	}
 
 	return Xchat::EAT_NONE;
