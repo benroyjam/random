3793c5b69b0423314e73202fb04f4792c9abcfc6
Arnav Singh <arnavion@gmail.com>
2013-08-02 00:10:48 -0700

    build-scripts: bitcoin: Don't build bitcoind.

--- build-scripts/bitcoin.sh
+++ build-scripts/bitcoin.sh
@@ -3,8 +3,8 @@
 cd `dirname $0`/../bitcoin/ &&
 git clean -xfd &&
 git reset --hard &&
-cd src/ &&
-make -j2 -f makefile.unix USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- DEBUGFLAGS= CFLAGS=-march=native CXXFLAGS=-march=native &&
-cd ../ &&
+# cd src/ &&
+# make -j2 -f makefile.unix USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- DEBUGFLAGS= CFLAGS=-march=native CXXFLAGS=-march=native &&
+# cd ../ &&
 qmake USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- QMAKE_CFLAGS=-march=native QMAKE_CXXFLAGS=-march=native &&
 make -j2
