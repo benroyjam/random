e4586140da6e426bb39cda8d2de61c44541ded20
Arnav Singh <arnavion@gmail.com>
2013-08-02 00:09:37 -0700

    build-scripts: hexchat: Don't enable XShm.

--- build-scripts/hexchat.sh
+++ build-scripts/hexchat.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`/../hexchat/ &&
 sh autogen.sh &&
-./configure --disable-checksum --disable-doat --disable-fishlim --enable-spell=none --enable-shm --enable-python=python3 CFLAGS=-march=native CXXFLAGS=-march=native &&
+./configure --disable-checksum --disable-doat --disable-fishlim --enable-spell=none --enable-python=python3 CFLAGS=-march=native CXXFLAGS=-march=native &&
 make clean &&
 make -j2 &&
 sudo make install
