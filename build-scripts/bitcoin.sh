473a1737c4b5be7e0e35e2c8abb796b5108a9076
Arnav Singh <arnavion@gmail.com>
2013-08-22 21:14:03 -0700

    build-scripts: bitcoin: Use Qt5's qmake.

--- build-scripts/bitcoin.sh
+++ build-scripts/bitcoin.sh
@@ -6,5 +6,5 @@ git reset --hard &&
 # cd src/ &&
 # make -j2 -f makefile.unix USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- DEBUGFLAGS= CFLAGS=-march=native CXXFLAGS=-march=native &&
 # cd ../ &&
-qmake USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- QMAKE_CFLAGS=-march=native QMAKE_CXXFLAGS=-march=native &&
+qmake-qt5 USE_DBUS=1 USE_IPV6=0 USE_QRCODE=0 USE_UPNP=- QMAKE_CFLAGS=-march=native QMAKE_CXXFLAGS=-march=native &&
 make -j2
