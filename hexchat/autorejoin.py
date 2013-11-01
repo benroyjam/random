91eba461f646e7f678435442fe99bb2ab8600332
Arnavion <arnavion@gmail.com>
2013-11-01 01:54:40 -0700

    autorejoin.py: Increased delay to safe levels.

--- hexchat/autorejoin.py
+++ hexchat/autorejoin.py
@@ -7,7 +7,7 @@ __module_description__ = ''
 hexchat.prnt('Autorejoin script loaded')
 
 def autorejoin_callback(word, word_eol, user_data):
-	hexchat.hook_timer(1500, rejoin_timer_callback, word[1])
+	hexchat.hook_timer(3000, rejoin_timer_callback, word[1])
 
 def rejoin_timer_callback(user_data):
 	hexchat.command('join {0}'.format(user_data))
